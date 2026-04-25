252f596552299c4b186fdc0b3f7e87b329bfde2e
latoiste <182464121+latoiste@users.noreply.github.com>
2026-04-25 21:49:54 +0700

    fix shadows on props

--- Assets/Scripts/Camera/Ysort.cs
+++ Assets/Scripts/Camera/Ysort.cs
@@ -1,17 +1,19 @@
+using System;
 using UnityEngine;
 
 public class YSort : MonoBehaviour
 {
-    [SerializeField] private int baseOrder = 1000;
+    [SerializeField, Range(1000, 5000)] private int baseOrder = 5000;
     private SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null) throw new Exception($"Attribute {this} cannot be null");
     }
 
     void LateUpdate()
     {
-        spriteRenderer.sortingOrder = (int)(baseOrder - transform.position.y * 100);
+        spriteRenderer.sortingOrder = (int)(baseOrder - transform.position.y * 100) % 1000;
     }
 }
