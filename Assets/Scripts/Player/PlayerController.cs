55615588d503ee072b3f28c5d87eeba3fc064d34
latoiste <182464121+latoiste@users.noreply.github.com>
2026-05-06 16:51:14 +0700

    add player animation for direction

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField, Range(1, 5)]
     private float moveSpeed;
+
+    private enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
     
-    private Rigidbody2D rb = null;
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
@@ -21,15 +34,23 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = direction * moveSpeed;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (rb.linearVelocityX > 0)
+        {
+            animator.SetInteger("Direction", (int)Direction.RIGHT);
+            sprite.flipX = true;
+        } else if (rb.linearVelocityX < 0)
+        {
+            animator.SetInteger("Direction", (int)Direction.LEFT);
+            sprite.flipX = false;
+        } 
+        else if (rb.linearVelocityY > 0)
+        {
+            animator.SetInteger("Direction", (int)Direction.UP);
+        } else if (rb.linearVelocityY < 0)
+        {
+            animator.SetInteger("Direction", (int)Direction.DOWN);
+        }
     }
 }
