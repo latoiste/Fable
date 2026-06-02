0e2aa8cd042d3504948e389e44d9ab7fe8e0b2d7
latoiste <182464121+latoiste@users.noreply.github.com>
2026-06-02 11:42:50 +0700

    add island scene template

---
+++ Assets/Scripts/TemplatePipeline/AddSceneListPipeline.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Profile;
+using UnityEditor.SceneManagement;
+using UnityEditor.SceneTemplate;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class AddSceneListPipeline : ISceneTemplatePipeline
+{
+    public virtual bool IsValidTemplateForInstantiation(SceneTemplateAsset sceneTemplateAsset)
+    {
+        return true;
+    }
+
+    public virtual void BeforeTemplateInstantiation(SceneTemplateAsset sceneTemplateAsset, bool isAdditive, string sceneName)
+    {
+        
+    }
+
+    public virtual void AfterTemplateInstantiation(SceneTemplateAsset sceneTemplateAsset, Scene scene, bool isAdditive, string sceneName)
+    {
+        List<EditorBuildSettingsScene> scenes = EditorBuildSettings.scenes.ToList();
+        int islandCount = 0;
+
+        foreach (var buildScene in scenes)
+        {
+            if (buildScene.path.Contains("Island") && buildScene.enabled) islandCount++;
+        }
+
+        string newSceneName = $"Island_{islandCount}";
+        string newScenePath = $"Assets/Scenes/Islands/{newSceneName}.unity";
+
+        Debug.Log(newScenePath);
+
+        EditorSceneManager.SaveScene(scene, newScenePath);
+
+        EditorBuildSettingsScene newBuildScene = new(newScenePath, true);
+        scenes.Add(newBuildScene);
+
+        EditorBuildSettings.scenes = scenes.ToArray();
+    }
+}
